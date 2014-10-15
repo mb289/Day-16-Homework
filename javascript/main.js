3614bfe2b92df5d190f77d1dc283a952787dde55
Marco Bernal Jr <bernalmjr@Marcos-MacBook-Air.local>
2014-10-14 19:53:24 -0500

    Updtes

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,21 @@
 var githubUsername = "mb289";
 var url = "https://api.github.com/users/" + githubUsername;
-$.get(url).then(function(data) {  
+$.get(url).then(function(data) {
     drawProfile(data);
 })
 
-function drawProfile(data) {  
-    document.body.innerHTML = [    
-        '<h1>',     
-        data.name,     
-        '</h1>',     
-        '<h2> blog:',     
-        data.blog,     
+function drawProfile(data) {
+    document.body.innerHTML = [
+       
+        '<h1>',
+        data.name,
+        '</h1>',
+        '<h2> blog:',
+        data.blog,
         '</h2>',
         '<h2> location:',
         data.location,
-        '/h2>',
+        '</h2>',
         '<h2> email:',
         data.email,
         '</h2>',
@@ -23,6 +24,6 @@ function drawProfile(data) {  
         '</h3>',
         '<h3> site:',
         data.html_url,
-        '</h3>'  
+        '</h3>'
     ].join('')
 }
